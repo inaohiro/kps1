931313c4bf1b0863c27122384c0032beb05f3999
Naohiro Iwamoto <40224016+inaohiro@users.noreply.github.com>
2023-07-05 12:31:39 +0900

    Update kps1.sh

--- kps1.sh
+++ kps1.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# oriinal kubeconfig
-__KPS1_ORIGINAL_KUBECONFIG=~/.kube/config
-
-# create temporary directory
-# this directory will be removed after terminal closed
-__KPS1_TMP_DIR="$(mktemp -dt kubeconfig.XXXXXX)"
-trap "rm -rf $__KPS1_TMP_DIR" EXIT
-
-# for terminal isolation, create temporary kubeconfig
-__KPS1_KUBECONFIG="${__KPS1_TMP_DIR}/config"
-export KUBECONFIG="${__KPS1_KUBECONFIG}"
-
-# variable for PS1
-__KPS1_CONTEXT=""
+__KPS1=""
 
 # color codes
 __KPS1_COLOR_MAGENTA="\e[35m"
@@ -21,74 +8,62 @@ __KPS1_COLOR_CYAN="\e[36m"
 __KPS1_COLOR_END="\e[0m"
 
 __kps1_init() {
-  # copy original kubeconfig
-  cp "${__KPS1_ORIGINAL_KUBECONFIG}" "${__KPS1_KUBECONFIG}"
+  local context=$(yq '.contexts[0].name' $HOME/.kube/config)
+  local namespace=$(yq '.contexts[0].context.namespace // "default"' $HOME/.kube/config)
 
-  # get current contexts
-  local context=$(kubectl config get-contexts --kubeconfig ${__KPS1_ORIGINAL_KUBECONFIG} | grep -e '\*' | tr -s ' ' | tr -d '\*')
+  alias __kps1='kubectl'
 
-  local cluster=$(echo $context | cut -f1 -d' ' -s)
-  local namespace=$(echo $context | cut -f4 -d' ' -s)
-  local namespace=${namespace:-default}
-
-  __kps1_set_context $cluster $namespace
+  __kps1_set_all $context $namespace
 }
 
 __kps1_get_context() {
-  printf "${__KPS1_CONTEXT}"
-}
-__kps1_get_cluster() {
-  # printf "${__KPS1_CONTEXT}" | cut -f1 -d':'
-  local context=(${__KPS1_CONTEXT//:/ })
+  # printf "${__KPS1}" | cut -f1 -d':'
+  local context=(${__KPS1//:/ })
   printf ${context[0]}
 }
 __kps1_get_namespace() {
-  # printf "${__KPS1_CONTEXT}" | cut -f2 -d':'
-  local context=(${__KPS1_CONTEXT//:/ })
+  # printf "${__KPS1}" | cut -f2 -d':'
+  local context=(${__KPS1//:/ })
   printf ${context[1]}
 }
-__kps1_set_context() {
-  __KPS1_CONTEXT="${1}:${2}"
+__kps1_set_all() {
+  __KPS1="${1}:${2}"
 }
-__kps1_set_cluster() {
+__kps1_set_context() {
   local namespace=$(__kps1_get_namespace)
-  __KPS1_CONTEXT="${1}:${namespace}"
+  __KPS1="${1}:${namespace}"
 }
 __kps1_set_namespace() {
-  local cluster=$(__kps1_get_cluster)
-  __KPS1_CONTEXT="${cluster}:${1}"
+  local context=$(__kps1_get_context)
+  __KPS1="${context}:${1}"
 }
 
 __kps1_ps1() {
-  echo -e "(${__KPS1_COLOR_CYAN}$(__kps1_get_cluster)${__KPS1_COLOR_END}:${__KPS1_COLOR_MAGENTA}$(__kps1_get_namespace)${__KPS1_COLOR_END})"
+  echo -e "(${__KPS1_COLOR_CYAN}$(__kps1_get_context)${__KPS1_COLOR_END}:${__KPS1_COLOR_MAGENTA}$(__kps1_get_namespace)${__KPS1_COLOR_END})"
 }
 
 __kps1_switch_context() {
-  # use original kubeconfig to get all contexts
-  local ctx=$(kubectl config get-contexts -o name --kubeconfig ${__KPS1_ORIGINAL_KUBECONFIG} | fzf)
-  if [[ -z "$ctx" ]]; then
+  local kubeconfig=$(ls -p $HOME/.kube/config.d | grep -v / | fzf --no-sort)
+  if [[ -z "$kubeconfig" ]]; then
     return
   fi
+  unalias __kps1
+  alias __kps1="kubectl --kubeconfig=$HOME/.kube/config.d/$kubeconfig"
 
-  # update context
-  local kubeconfig=$(kubectl config view --flatten --merge --output json --kubeconfig ${__KPS1_ORIGINAL_KUBECONFIG} | jq -c --arg ctx $ctx '(.contexts[] | select(.name == $ctx)) as $c | {apiVersion, kind, preferences, "current-context": $ctx, contexts: [$c], clusters: [.clusters[] | select(.name == $c.context.cluster)], users: [.users[] | select(.name == $c.context.user)]}')
+  local context=$(yq '.contexts[0].name' $HOME/.kube/config.d/$kubeconfig)
+  local namespace=$(yq '.contexts[0].context.namespace // "default"' $HOME/.kube/config.d/$kubeconfig)
 
-  (echo $kubeconfig > $__KPS1_KUBECONFIG &)
-  local context=$(echo $kubeconfig | jq -c -r '[.clusters[0].name, .contexts[0].context.namespace // "default"] | join(" ")')
-  local cluster=$(echo $context | cut -f1 -d' ' -s)
-  local namespace=$(echo $context | cut -f2 -d' ' -s)
-
-  __kps1_set_context $cluster $namespace
+  __kps1_set_all $context $namespace
 }
 
 __kps1_switch_namespace() {
-  local ns=$(kubectl get namespace -o name | cut -f2 -d'/' | fzf)
+  local ns=$(__kps1 get namespace -o name | cut -f2 -d'/' | fzf)
   if [[ -z "${ns}" ]]; then
     return
   fi
+  (__kps1 config set-context --current --namespace="${ns}" > /dev/null &)
 
   # update namespace
-  (kubectl config set-context --current --namespace="${ns}" > /dev/null &)
   __kps1_set_namespace "${ns}"
 }
 
