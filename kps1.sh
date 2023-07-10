2772774407bd8b26f19285a7ae29b67dfe0464dd
Naohiro Iwamoto <40224016+inaohiro@users.noreply.github.com>
2023-07-10 13:33:39 +0900

    Update kps1.sh

--- kps1.sh
+++ kps1.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 __KPS1=""
+__KPS1_KUBECONFIG="$HOME/.kube/config"
+__KPS1_KUBECONFIG_DIR="$HOME/.kube/config.d"
 
 # color codes
 __KPS1_COLOR_MAGENTA="\e[35m"
 __KPS1_COLOR_CYAN="\e[36m"
 __KPS1_COLOR_END="\e[0m"
 
+__kps1_kubectl() {
+  kubectl --kubeconfig="${__KPS1_KUBECONFIG}" $@
+}
+
 __kps1_init() {
   local context=$(yq '.contexts[0].name' $HOME/.kube/config)
-  local namespace=$(yq '.contexts[0].context.namespace // "default"' $HOME/.kube/config)
+  local namespace=$(yq '.contexts[0].context.namespace // "default"' ${__KPS1_KUBECONFIG})
 
-  alias __kps1='kubectl'
+  # alias __kps1='kubectl'
 
   __kps1_set_all $context $namespace
 }
@@ -37,20 +43,24 @@ __kps1_set_namespace() {
   local context=$(__kps1_get_context)
   __KPS1="${context}:${1}"
 }
+__kps1_set_kubeconfig() {
+  __KPS1_KUBECONFIG=${1}
+}
 
 __kps1_ps1() {
   echo -e "(${__KPS1_COLOR_CYAN}$(__kps1_get_context)${__KPS1_COLOR_END}:${__KPS1_COLOR_MAGENTA}$(__kps1_get_namespace)${__KPS1_COLOR_END})"
 }
 
 __kps1_switch_context() {
-  local kubeconfig=$(ls -p $HOME/.kube/config.d | grep -v / | fzf --no-sort)
+  local kubeconfig=$(ls -p $__KPS1_KUBECONFIG_DIR | grep -v / | fzf --no-sort)
   if [[ -z "$kubeconfig" ]]; then
     return
   fi
+  __kps1_set_kubeconfig "$__KPS1_KUBECONFIG_DIR/$kubeconfig"
   unalias __kps1
-  alias __kps1="kubectl --kubeconfig=$HOME/.kube/config.d/$kubeconfig"
+  # alias __kps1="kubectl --kubeconfig=$__KPS1_KUBECONFIG_DIR/$kubeconfig"
 
-  local context=$(yq '.contexts[0].name' $HOME/.kube/config.d/$kubeconfig)
+  local context=$(yq '.contexts[0].name' $__KPS1_KUBECONFIG_DIR/$kubeconfig)
   local namespace=$(yq '.contexts[0].context.namespace // "default"' $HOME/.kube/config.d/$kubeconfig)
 
   __kps1_set_all $context $namespace
@@ -61,7 +71,7 @@ __kps1_switch_namespace() {
   if [[ -z "${ns}" ]]; then
     return
   fi
-  (__kps1 config set-context --current --namespace="${ns}" > /dev/null &)
+  (kubectl --kubeconfig "${__KPS1_KUBECONFIG}" config set-context --current --namespace="${ns}" > /dev/null &)
 
   # update namespace
   __kps1_set_namespace "${ns}"
